356bcc9ed8b532840064351c33e37a251d01e7a4
DavidG <datog309@gmail.com>
2021-07-30 18:50:05 +0400

    first ver

---
+++ src/component/ChekoutProgressBar/CheckoutProgressBar.component.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { 
+    BILLING_STEP,
+    DETAILS_STEP,
+    SHIPPING_STEP    
+} from "Route/Checkout/Checkout.config";
+
+import "./CheckoutProgressBar.style"
+
+export class CheckoutProgressBar extends React.PureComponent {
+    constructor(props){
+        super(props);
+        this.renderSteps = this.renderSteps.bind(this);
+    }
+
+    stepIndex = {
+        [SHIPPING_STEP]: 1,
+        [BILLING_STEP]: 2,
+        [DETAILS_STEP]: 3
+    }
+
+    renderSteps(stepEntry, index) {
+        const [key, value] = stepEntry;
+        const { checkoutStep } = this.props;
+        const isCompleted = this.stepIndex[checkoutStep] > this.stepIndex[key] || checkoutStep === DETAILS_STEP;
+        return (
+            <div className={`step ${checkoutStep === key && "active"} ${isCompleted && "completed"}`}>
+                <div className="white-margin">
+                    <span className="step-index">
+                        {
+                            isCompleted ? <>&#10003;</> : index + 1
+                        }
+                    </span>
+                </div>
+                <span className="step-name">{value.title}</span>
+            </div>
+        )
+    }
+
+    render() {
+        const { stepMap, progress } = this.props;
+        const stepMapEntries = Object.entries(stepMap);
+        return (
+            <div className="Progress-bar">
+                <div className="bar"></div>
+                <div className="bar-completed" style={{ width: this.props.progress }}></div>
+                { stepMapEntries.map(this.renderSteps) }
+            </div>
+        )
+    }
+}
+
+export default CheckoutProgressBar;
---
+++ src/route/Checkout/Checkout.component.js
@@ -0,0 +1,54 @@
+import ContentWrapper from "Component/ContentWrapper";
+import {
+    Checkout as SourceCheckout
+} from "SourceRoute/Checkout/Checkout.component";
+import { 
+    BILLING_STEP,
+    DETAILS_STEP,
+    SHIPPING_STEP    
+} from "Route/Checkout/Checkout.config";
+
+import CheckoutProgressBar from "Component/ChekoutProgressBar/CheckoutProgressBar.component";
+
+export class Checkout extends SourceCheckout {
+    renderProgressBar() {
+        const { checkoutStep } = this.props;
+        return <CheckoutProgressBar checkoutStep={checkoutStep} stepMap={this.stepMap} progress={this.progressWidth[checkoutStep]} />
+    }
+
+    progressWidth = {
+        [SHIPPING_STEP]: "15%",
+        [BILLING_STEP]: "46%",
+        [DETAILS_STEP]: "100%"
+    }
+
+
+    render() {
+        return (
+            <>
+            <main block="Checkout">
+                {this.renderProgressBar()}
+                <ContentWrapper
+                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+                  label={ __('Checkout page') }
+                  >
+                    { this.renderSummary(true) }
+                    <div block="Checkout" elem="Step">
+                        { this.renderTitle() }
+                        { this.renderGuestForm() }
+                        { this.renderStep() }
+                        { this.renderLoader() }
+                    </div>
+                    <div>
+                        { this.renderSummary() }
+                        { this.renderPromo() }
+                        { this.renderCoupon() }
+                    </div>
+                </ContentWrapper>
+            </main>
+            </>
+        )
+    }
+}
+
+export default Checkout;
